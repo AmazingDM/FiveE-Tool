a010bf6b3b0bda70218b38f1997a9905be46919e
Amazing_DM <amazingdmdd@gmail.com>
2020-06-15 15:26:08 +0800

    update to 0.4

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         估算5E天梯分
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Thanks https://thagki9.com/5e/!
+// @version      0.4
+// @description  算法来自 @THaGKI9 thagki9.com/5e
 // @author       Amaz1ngDM
 // @include      https://www.5ewin.com/data/player/*
 // @require http://code.jquery.com/jquery-3.5.0.min.js
@@ -13,13 +13,17 @@
 
     var uid = window.location.href.split("data/player/")[1];
 
+    var svip_eloElm =$('.elo');
     var eloElm =$('.elo-word');
-    var eloElmOriText =$('.elo-word').text();
+
+    if(eloElm.length==0)
+        eloElm = svip_eloElm
+    var eloElmOriText =eloElm.text();
     $.getJSON("https://1217741033825494.cn-shenzhen.fc.aliyuncs.com/2016-08-15/proxy/FiveEEloLookup/InferElo/?id="+uid+"&t="+new Date(),function(data){
         if(data.success){
-            eloElm.text(eloElmOriText+"(估算"+data.data.elo+")");
+            eloElm.text(eloElmOriText+"("+data.data.elo+")");
         }else{
-            console.log("估算5E天梯分API返回错误")
+            eloElm.text(eloElmOriText+"(API错误)");
         }
     });
 })();
